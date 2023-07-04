9d85ebc9171c9dfbc1fab569162279d931bb864c
V P <vpernicka@gmail.com>
2023-07-04 15:17:56 +0200

    tests, project

--- QuantiTests/RocketTests.swift
+++ QuantiTests/RocketTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Quanti_test
 
 final class RocketTests: XCTestCase {
 
@@ -22,20 +23,118 @@ final class RocketTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSingleRocketDecode() throws {
+      let result = try JSONDecoder().decode([Rocket].self, from: jsonData)
+      XCTAssert(result.count == 1)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
+
+
+let jsonData = """
+[
+    {
+        "id": 1,
+        "active": false,
+        "stages": 2,
+        "boosters": 0,
+        "cost_per_launch": 6700000,
+        "success_rate_pct": 40,
+        "first_flight": "2006-03-24",
+        "country": "Republic of the Marshall Islands",
+        "company": "SpaceX",
+        "height": {
+            "meters": 22.25,
+            "feet": 73
+        },
+        "diameter": {
+            "meters": 1.68,
+            "feet": 5.5
+        },
+        "mass": {
+            "kg": 30146,
+            "lb": 66460
+        },
+        "payload_weights": [
+            {
+                "id": "leo",
+                "name": "Low Earth Orbit",
+                "kg": 450,
+                "lb": 992
+            }
+        ],
+        "first_stage": {
+            "reusable": false,
+            "engines": 1,
+            "fuel_amount_tons": 44.3,
+            "burn_time_sec": 169,
+            "thrust_sea_level": {
+                "kN": 420,
+                "lbf": 94000
+            },
+            "thrust_vacuum": {
+                "kN": 480,
+                "lbf": 110000
+            }
+        },
+        "second_stage": {
+            "reusable": false,
+            "engines": 1,
+            "fuel_amount_tons": 3.38,
+            "burn_time_sec": 378,
+            "thrust": {
+                "kN": 31,
+                "lbf": 7000
+            },
+            "payloads": {
+                "option_1": "composite fairing",
+                "composite_fairing": {
+                    "height": {
+                        "meters": 3.5,
+                        "feet": 11.5
+                    },
+                    "diameter": {
+                        "meters": 1.5,
+                        "feet": 4.9
+                    }
+                }
+            }
+        },
+        "engines": {
+            "number": 1,
+            "type": "merlin",
+            "version": "1C",
+            "layout": "single",
+            "isp": {
+                "sea_level": 267,
+                "vacuum": 304
+            },
+            "engine_loss_max": 0,
+            "propellant_1": "liquid oxygen",
+            "propellant_2": "RP-1 kerosene",
+            "thrust_sea_level": {
+                "kN": 420,
+                "lbf": 94000
+            },
+            "thrust_vacuum": {
+                "kN": 480,
+                "lbf": 110000
+            },
+            "thrust_to_weight": 96
+        },
+        "landing_legs": {
+            "number": 0,
+            "material": null
+        },
+        "flickr_images": [
+            "https://imgur.com/DaCfMsj.jpg",
+            "https://imgur.com/azYafd8.jpg"
+        ],
+        "wikipedia": "https://en.wikipedia.org/wiki/Falcon_1",
+        "description": "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.",
+        "rocket_id": "falcon1",
+        "rocket_name": "Falcon 1",
+        "rocket_type": "rocket"
+    }
+]
+""".data(using: .utf8)!
--- QuantiTests/SpaceXAPITests.swift
+++ QuantiTests/SpaceXAPITests.swift
@@ -6,9 +6,13 @@
 //
 
 import XCTest
+@testable import Quanti_test
+import Combine
 
 final class SpaceXAPITests: XCTestCase {
 
+  var cancellables = Set<AnyCancellable>()
+  
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,19 +21,39 @@ final class SpaceXAPITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() async {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+      
+      
+      let api = SpaceXAPI()
+      let expectation = self.expectation(description: "Result")
+      let expectationFinished = self.expectation(description: "Finished")
+      api.getAllRockets().sink { completed in
+        switch completed {
+          case .failure(let error):
+            XCTFail("api error: \(error)")
+        case .finished:
+          expectationFinished.fulfill()
+        }
+      } receiveValue: { rockets in
+        print(rockets)
+        XCTAssert(rockets.count > 2)
+        expectation.fulfill()
+      }
+      .store(in: &cancellables)
+
+      await waitForExpectations(timeout: 5, handler: nil)
+
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+      
     }
 
 }
+
+
